961387c38af058d41da1dc32ebfa49c9699c9334
Jack Clarke <jackojc@gmail.com>
2021-02-08 22:30:53 +0000

    Improved testing infrastructure - Updated the run_test.py script to be quite a bit more robust - Add some new tests

--- tests/run_test.py
+++ tests/run_test.py
@@ -23,20 +23,70 @@ if __name__ == "__main__":
 		print("usage: <test.wpp> <w++ exe>")
 		sys.exit(1)
 
+	# Unpack argv
 	_, test_file, binary = sys.argv
 
-	# What is the correct output expected to be?
-	correct_output = run([binary, test_file]).decode("UTF-8").split('\n')
+
+	# Run wot++ and get output.
+	wpp_output = run([binary, test_file]).decode("UTF-8").strip()
+
+
+	# Find all test cases of the form `#[expect(foo)]`
+	matches = []
 
 	with open(test_file, 'r') as f:
-		i = 0
+		test_file = f.read()
 
-		for j, line in enumerate(f.readlines()):
-			m = re.search("(?<=\#\[ expect: ).*(?= \])", line.rstrip())
+	match_iter = re.finditer(r"(?:#\[\s*expect\()(\w+)(?:\)\s*\])", test_file)
 
-			if m is not None:
-				if m.group(0) != correct_output[i]:
-					print(f"mismatch, got '{correct_output[i]}', expected '{m.group(0)}' (line {j + 1})")
-					sys.exit(1)
+	if match_iter is not None:
+		for m in match_iter:
+			matches.append(m)
 
-				i += 1
+
+	# Compare the expected output to the actual output.
+	ptr = 0
+	for m in matches:
+		test = m.group(1)
+		length = len(test)
+
+		actual = wpp_output[ptr : ptr + length]
+
+
+		# Check for mismatch.
+		if actual != test:
+			start, end = m.span()   # Byte offset of comparison that failed.
+			line = column = 1
+
+
+			# Loop through characters in `test_file` and keep track of byte offset.
+			for byte_offset, char in enumerate(test_file):
+				# Break if we're at the location of the comparison that failed.
+				if byte_offset == start:
+					break
+
+				# Track line number and column.
+				if char == '\n':
+					line += 1
+					column = 1
+
+				else:
+					column += 1
+
+
+			# Print error location and the error itself.
+			print(f"expect @ {line}:{column} failed!")
+			print(f" -> expected '{actual}', got '{test}'.")
+
+
+			# Return non-zero status.
+			sys.exit(1)
+
+
+		# Advance window by length of `test`.
+		ptr += length
+
+
+
+
+
