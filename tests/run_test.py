9f30fb8f2468b5ebb1a6111b70c99f3c32216616
Jack Clarke <jackojc@gmail.com>
2021-02-15 02:15:48 +0000

    run_test.py checks if output is non-empty now and fixed some tests

--- tests/run_test.py
+++ tests/run_test.py
@@ -39,8 +39,9 @@ if __name__ == "__main__":
 
 	try:
 		wpp_output = run([binary, test_file])
-		if wpp_output[-1] == '\n':
-			wpp_output = wpp_output[:-1]
+		if len(wpp_output) > 0:
+			if wpp_output[-1] == '\n':
+				wpp_output = wpp_output[:-1]
 
 	except RuntimeError as err:
 		print(f"w++ failed: {err.args[0]}")
