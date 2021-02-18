4d56921c7077a4ea7ca2a6cb758b7ee41e858ad6
Jacob Lagares Pozo <jlagarespo@iebesalu.cat>
2021-02-19 00:55:43 +0100

    Warn the user when functions are getting shadowed by parameters. (#53)
    * Warn the user when functions are getting shadowed by parameters.
    
    * Fix
    
    * Fix tests
    
    * Don't refer to mangled function name in shadow warning
    
    Co-authored-by: Jack Clarke <jackojc@gmail.com>

--- src/visitors/eval.hpp
+++ src/visitors/eval.hpp
@@ -337,11 +337,15 @@ namespace wpp {
 				// Check if parameter.
 				if (args) {
 					if (auto it = (*args).find(caller_name); it != (*args).end()) {
-						if (caller_args.size() > 0) {
+						if (caller_args.size() > 0)
 							throw wpp::Exception{caller_pos, "calling '", caller_name, "' as if it were a function, it is an argument."};
-						}
 
 						str = it->second;
+
+						// Check if it's shadowing a function (even this one).
+						if (functions.find(it->first + "0") != functions.end())
+							wpp::warn(caller_pos, "parameter ", caller_name, " is shadowing a function");
+
 						return;
 					}
 				}
