11606a9ca1363b6ee404dcd2f173cc2a09ad6c34
Violetas <violet.hates.glowies@protonamail.com>
2021-02-24 17:25:44 +0000

    Added --lint argument

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@
 #include <misc/argp.hpp>
 
 int main(int argc, const char* argv[]) {
-	wpp::ArgResult input, output, sexpr, repl;
+	wpp::ArgResult input, output, sexpr, repl, lint;
 
 	auto usage = "w++ -i INPUT [-o OUTPUT] [-srh]";
 
@@ -23,22 +23,35 @@ int main(int argc, const char* argv[]) {
 		"alpha-git",
 		usage
 	)
-		.arg(&input,  "File to read input from",               "input",  "i", true)
-		.arg(&output, "File to output to (stdout by default)", "output", "o", true)
-		.arg(&sexpr,  "Print AST as S-expression",             "sexpr",  "s", false)
-		.arg(&repl,   "Start an interactive prompt",           "repl",   "r", false);
+		.arg(&input,  "File to read input from",                        "input",  "i", true)
+		.arg(&output, "File to output to (stdout by default)",          "output", "o", true)
+		.arg(&sexpr,  "Print AST as S-expression",                      "sexpr",  "s", false)
+		.arg(&repl,   "Start an interactive prompt",                    "repl",   "r", false)
+		.arg(&lint,   "Enable various warnings (comma-seperated list)", "lint",   "l", true);
 
-	wpp::warning_t warning_flags =
-		wpp::WARN_FUNC_REDEFINED |
-		wpp::WARN_PARAM_SHADOW_FUNC |
-		wpp::WARN_PARAM_SHADOW_PARAM |
-		wpp::WARN_VARFUNC_REDEFINED
-	;
+	wpp::warning_t warning_flags;
 
 	if (not argparser.parse(argc, argv))
 		return 1;
 
-	else if (repl.is_present)
+	// Enable any lints
+	if (lint.is_present) {
+		std::istringstream list(lint.value);
+		std::string buf;
+
+		while (std::getline(list, buf, ',')) {
+			if      (buf == "param-shadow-func")
+				warning_flags |= wpp::WARN_PARAM_SHADOW_FUNC;
+			else if (buf == "param-shadow-param")
+				warning_flags |= wpp::WARN_PARAM_SHADOW_PARAM;
+			else if (buf == "func-redefined")
+				warning_flags |= wpp::WARN_FUNC_REDEFINED;
+			else if (buf == "varfunc-redefined")
+				warning_flags |= wpp::WARN_VARFUNC_REDEFINED;
+		}
+	}
+
+	if (repl.is_present)
 		return wpp::repl();
 
 	else if (input.is_present)
--- src/misc/argp.hpp
+++ src/misc/argp.hpp
@@ -148,6 +148,8 @@ namespace wpp {
 				return true;
 			}
 	};
+
+
 }
 
 #endif
