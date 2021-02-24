151a508db1d2f2b436dfd350ad8e8c5c6e7d15d4
Violetas <violet.hates.glowies@protonamail.com>
2021-02-24 15:36:46 +0000

    Add extra options and modify help

--- src/main.cpp
+++ src/main.cpp
@@ -15,24 +15,24 @@
 
 int main(int argc, const char* argv[]) {
 	wpp::ArgResult input, output, sexpr, repl;
-	
-	auto usage = "w++ -i INPUT [-o OUTPUT] [-sh]";
+
+	auto usage = "w++ -i INPUT [-o OUTPUT] [-srh]";
 
 	auto argparser = wpp::ArgumentParser(
-		"wot++", 
-		"A small macro language for producing and manipulating strings", 
-		"alpha-git", 
+		"wot++",
+		"A small macro language for producing and manipulating strings",
+		"alpha-git",
 		usage
 	)
 		.arg(&input,  "File to read input from",               "input",  "i", true)
-//		.arg(&output, "File to output to (stdout by default)", "output", "o", true)
-//		.arg(&sexpr,  "Print AST as S-expression",             "sexpr",  "s", false)
+		.arg(&output, "File to output to (stdout by default)", "output", "o", true)
+		.arg(&sexpr,  "Print AST as S-expression",             "sexpr",  "s", false)
 		.arg(&repl,   "Start an interactive prompt",           "repl",   "r", false);
 
-	if (!argparser.parse(argc, argv)) 
-		return -1;
+	if (not argparser.parse(argc, argv))
+		return 1;
 
-	if (repl.is_present)
+	else if (repl.is_present)
 		return wpp::repl();
 
 	else if (input.is_present)
